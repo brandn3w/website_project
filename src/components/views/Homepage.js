4e3983523876e561b9324ea6e69aa94b820bc6fb
Nadja Brandys <nadiusza@gmail.com>
2020-08-03 21:56:58 +0200

    Add hopepage

---
+++ src/components/views/Homepage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import PropTypes from 'prop-types';
+
+class Homepage extends React.Component {
+    static PropTypes = {}
+}
+
+render(){
+
+    return(
+<main>
+    <h1>Welcome</h1>
+</main>
+    )
+}
+
+export default Homepage;
