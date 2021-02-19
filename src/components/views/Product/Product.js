71fdb1d09070db2b80eda3fb6f849a6a6e3779bb
Nadja Brandys <nadiusza@gmail.com>
2021-02-20 00:29:47 +0100

    Styled shop section

--- src/components/views/Product/Product.js
+++ src/components/views/Product/Product.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button } from 'react-bootstrap'
 import { Card } from 'react-bootstrap';
 import Col from 'react-bootstrap/Col';
+//import Row from 'react-bootstrap/Row'
 //import { data } from '../../../data'
 import styles from './Product.module.scss';
 
@@ -9,16 +10,16 @@ const ProductShop = ({product}) => (
 
 
   <Col xs={12} md={6} lg={3} key={product.id} > 
-    <Card style={{ width: '18rem', height: '100%'}}>      
-      <Card.Header>{product.title}</Card.Header>
-      <Card.Img  variant="top" src={product.image} style={{height:'100%', objectFit:'cover'}}/>
+    <Card style={{ width: '18rem'}}>      
+      <Card.Img  variant="top" src={product.image} style={{height: 250 , objectFit:'cover'}}/>
       <Card.Body>
-        <Card.Title>{product.title}</Card.Title>
+        <Card.Title style={{height: 40}}>{product.title}</Card.Title>
         <Button variant="outline-secondary">Add to cart</Button>
         <Button variant="outline-secondary">Add to favs</Button>
       </Card.Body>
     </Card> 
   </Col>
+
 )
 
 export default ProductShop;
